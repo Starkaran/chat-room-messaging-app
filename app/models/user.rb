f9238c0a3e095121b053e3f490fd3e4a3636109f
Starkaran <711706@student.nitandhra.ac.in>
2023-04-28 12:25:55 +0530

    git init

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+  validates_uniqueness_of :username
+
+  def self.generate
+    adjectives = ['Ancient', 'Broken', 'Creative', 'Dangerous', 'Effective', 'Flying', 'Gilded']
+    nouns = ['Highway', 'Intern', 'Liger', 'Tion', 'Master']
+    number = rand.to_s[2..4]
+
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+
+    create(username: username)
+  end
+end
